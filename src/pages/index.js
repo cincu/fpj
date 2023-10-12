d4cf4ed6f4055b9b080c963935e11d322138976b
CANSU CANER <CANANCANSUCANER@gmail.com>
2023-10-12 15:15:34 +0200

    page#0 launch, logo_1.png import, contact and works paths

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import Link from "next/link";
-export default function LogoPage() {
+import styled from "styled-components";
+import Image from "next/image";
+import logo from "../images/logo_1.png";
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3rem;
+`;
+export default function Home() {
   return (
-    <>
-      <h1>logo page</h1>
-      <Link href="./home">
-        <img alt="logo"></img>
-      </Link>
-    </>
+    <Div>
+      <Image src={logo} width={700} alt="Logo Png" />
+      <Link href="./contact-me">contact</Link>
+      <Link href="./works">works</Link>
+    </Div>
   );
 }
